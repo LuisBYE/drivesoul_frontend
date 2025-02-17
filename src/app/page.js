a9dc1b2753dd3357ef3b41a921dbab5c1a49057b
uarjona <unaxarjona@ceroca.cat>
2025-02-17 17:15:14 +0100

    LOGIN

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Menu from "./component/Menu.js"
+import Vlogin from "./Pages/Vlogin.js"
 
 export default function Home() {
   return (
     // <Menu> </Menu>
 
     <div>
-      <h1> Portada de Incio Coches</h1>
+      <h1> Portada de Incio Coche asas</h1>
+      <Vlogin></Vlogin>
     </div>
   );
 }
